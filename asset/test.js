624f3fa5a9c4e36ed98937d5081d008c47aa99bf
Quan Do <19qd1@tcl217a-10.cs.williams.edu>
2017-01-22 20:39:25 -0500

    done with timeLeft and nextOpen

--- asset/test.js
+++ asset/test.js
@@ -4,11 +4,16 @@ window.onload = function() {
   var fall = new Time(9,11);
   fall.setDays([12,22],[8,22],[8,22],[8,22],[8,22],[8,22],[10,17]);
   var spring = new Time(1,5);
-  spring.setDays([12,23],[9,23],[9,23],[9,23],[9,23],[9,23],[11,20]);
+  spring.setDays([12,20],[9,23],[9,23],[9,23],[9,23],[9,23],[11,20]);
   var summer = new Time(6,8);
 
   loc.update('Sawyer', [fall,spring,summer], 'studying');
   console.log(loc);
-  console.log(new Date());
-  console.log(loc.isOpen(new Date()));
+  var date = new Date();
+  date.setFullYear(2016);
+  date.setDate(29);
+  date.setMonth(1);
+  console.log(date);
+  console.log(loc.isOpen(date));
+  console.log(loc.nextOpen(date));
 }
